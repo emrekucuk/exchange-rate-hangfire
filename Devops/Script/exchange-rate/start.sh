aa27c904b99bc263fe0d7a661c54552158dc4602
Emre Küçük <emre.kucuk@crosstech.com.tr>
2022-09-25 14:39:45 +0300

    Devops: Created Jenkinsfile

--- Devops/Script/exchange-rate/start.sh
+++
@@ -1,5 +0,0 @@
-docker run -d \
-    --name exchange-rate \
-    -p 5001:80 \
-    -e PostgresConnection="User ID=postgres;Password=postgres;Server=192.168.2.194;Port=5432;Database=currency-exchange;" \
-    exchange-rate 
