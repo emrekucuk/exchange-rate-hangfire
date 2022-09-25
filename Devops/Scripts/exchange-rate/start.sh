0c477b38375f899ba814e339f057f3539955471c
Emre Küçük <emre.kucuk@crosstech.com.tr>
2022-09-25 15:01:56 +0300

    Devops: Updated Exchange-rate Start script

--- Devops/Scripts/exchange-rate/start.sh
+++ Devops/Scripts/exchange-rate/start.sh
@@ -1,5 +1,5 @@
 docker run -d \
     --name exchange-rate \
-    -p 5001:80 \
+    -p 5032:80 \
     -e PostgresConnection="User ID=postgres;Password=postgres;Server=localhost;Port=5432;Database=currency-exchange;" \
     exchange-rate 
