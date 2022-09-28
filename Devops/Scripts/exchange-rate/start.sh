1e9b5dbb0993e2b82adb8b5384493977b89e8f8c
Emre Küçük <emre.kucuk@crosstech.com.tr>
2022-09-28 11:25:19 +0300

    Devops: Updated Jenksinfile

--- Devops/Scripts/exchange-rate/start.sh
+++ Devops/Scripts/exchange-rate/start.sh
@@ -2,4 +2,4 @@ docker run -d \
     --name exchange-rate \
     -p 5032:80 \
     -e PostgresConnection="User ID=postgres;Password=postgres;Server=localhost;Port=5432;Database=currency-exchange;" \
-    exchange-rate 
+    registry.dev.crosstech.com.tr/exchange-rate 
