5462dfc7e9dcef6f692444e79e79fd34bb6df4ab
Emre Küçük <emre.kucuk@crosstech.com.tr>
2022-10-03 22:57:56 +0300

    Test Commit

--- Devops/Scripts/exchange-rate/start.sh
+++ Devops/Scripts/exchange-rate/start.sh
@@ -1,5 +1,5 @@
 docker run -d \
     --name exchange-rate \
     -p 5032:80 \
-    -e PostgresConnection="User ID=postgres;Password=postgres;Server=192.168.1.115;Port=5432;Database=currency-exchange;" \
+    -e PostgresConnection="User ID=postgres;Password=EeDReSw8cX5@Ceg&+JTst3FFgeCfPRBJNYF@X!N4zq2vB4F*;Server=141.98.1.177;Port=5432;Database=currency-exchange;" \
     registry.dev.crosstech.com.tr/exchange-rate 
